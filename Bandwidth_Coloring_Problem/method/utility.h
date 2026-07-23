2bb3e72c119af3ea54a6a3f4289eac25b5014f36
Vinh-Duy <duy.buivinh2007@gmail.com>
2026-07-23 22:58:22 +0700

    Utility Method BCP

---
+++ Bandwidth_Coloring_Problem/method/utility.h
@@ -0,0 +1,112 @@
+#ifndef BCP_BMCP_UTILITY_H
+#define BCP_BMCP_UTILITY_H
+
+#include <limits>
+#include <string>
+#include <vector>
+
+#include <cstddef>
+
+namespace SATSolver {
+    enum SOLVER { CADICAL };
+}
+
+namespace BCPSolver
+{
+    enum SolverStatus
+    {
+        UNKNOWN = -1,
+        UNSATISFIABLE = 0,
+        SATISFIABLE = 1,
+        OPTIMAL = 2,
+    };
+
+    enum SolvingMethod
+    {
+        TwoVariablesGreater,
+        TwoVariablesLess,
+        OneVariableGreater,
+        OneVariableLess,
+        StaircaseWithAuxiliaryVarsNoCache,
+        StaircaseWithAuxiliaryVarsWithCache,
+        StaircaseWithoutAuxiliaryVars
+    };
+
+    static constexpr double NO_TIME_LIMIT = std::numeric_limits<double>::lowest();
+
+    class Graph
+    {
+    private:
+        std::vector<std::tuple<int, int, int>> edges_list{};
+        std::vector<std::vector<int>> matrix{};
+        int n{};
+
+    public:
+        explicit Graph(const int n)
+            : edges_list(std::vector<std::tuple<int, int, int>>()), matrix(std::vector(n, std::vector<int>(n))),
+              n(n)
+        {
+        }
+
+        void add_edge(int i, int j, int w);
+
+        [[nodiscard]] const std::vector<std::tuple<int, int, int>>& get_edges() const;
+
+        [[nodiscard]] int get_weight(int i, int j) const;
+
+        [[nodiscard]] int get_number_of_nodes() const;
+
+        [[nodiscard]] int get_number_of_edges() const;
+
+        [[nodiscard]] int get_highest_degree_vertex() const;
+
+        [[nodiscard]] int get_degree(int node) const;
+
+        [[nodiscard]] std::vector<int> get_neighbors(const int node) const
+        {
+            std::vector<int> neighbors;
+            for (int j = 0; j < n; j++)
+            {
+                if (matrix[node][j] != 0)
+                {
+                    neighbors.push_back(j);
+                }
+            }
+            return neighbors;
+        }
+    };
+
+    Graph* read_bcp_graph(const std::string& file_path);
+
+    struct ProgramConfig
+    {
+        std::string filename;
+        double time_limit;
+        int upper_bound;
+        bool find_optimal;
+        bool incremental_mode;
+        std::string variable_for_incremental;
+        bool use_symmetry_breaking;
+        bool use_pairwise;
+        std::string width;
+        SATSolver::SOLVER solver;
+        SolvingMethod solving_method;
+        // Constructor with defaults
+        ProgramConfig()
+            : time_limit(NO_TIME_LIMIT), upper_bound(-1), find_optimal(true), incremental_mode(false),
+              use_symmetry_breaking(false), use_pairwise(false), solver(SATSolver::CADICAL),
+              solving_method(TwoVariablesGreater)
+        {
+        }
+    };
+
+    class ArgParser
+    {
+    public:
+        static void printUsage(const char* programName);
+
+        static ProgramConfig parse(int argc, char* argv[]);
+    };
+} // namespace BCPSolver
+
+#endif // BCP_BMCP_UTILITY_H
